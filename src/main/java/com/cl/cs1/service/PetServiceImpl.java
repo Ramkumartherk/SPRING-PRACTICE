5604322614062111e529631e620342cc428c38f9
RAMKUMAR S <118542194+Ramkumartherk@users.noreply.github.com>
2023-09-29 08:44:18 +0530

    Update PetServiceImpl.java

--- src/main/java/com/cl/cs1/service/PetServiceImpl.java
+++ src/main/java/com/cl/cs1/service/PetServiceImpl.java
@@ -6,10 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hcl.cs1.dao.PetDAO;
-import com.hcl.cs1.dao.personalpetDAO;
-import com.hcl.cs1.model.Pet;
-import com.hcl.cs1.model.personalpets;
+import com.cl.cs1.dao.PetDAO;
+import com.cl.cs1.dao.personalpetDAO;
+import com.cl.cs1.model.Pet;
+import com.cl.cs1.model.personalpets;
 
 @Service
 public class PetServiceImpl implements PetService {
