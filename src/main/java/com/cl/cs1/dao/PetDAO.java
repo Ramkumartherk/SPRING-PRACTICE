209ad38e835a92d947466d50d40a92c075d390a7
RAMKUMAR S <118542194+Ramkumartherk@users.noreply.github.com>
2023-09-29 08:35:06 +0530

    Update PetDAO.java

--- src/main/java/com/cl/cs1/dao/PetDAO.java
+++ src/main/java/com/cl/cs1/dao/PetDAO.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository; 
 
-import com.hcl.cs1.model.Pet;
-import com.hcl.cs1.model.User;
+import com.cl.cs1.model.Pet;
+import com.cl.cs1.model.User;
 
 public interface PetDAO extends JpaRepository<Pet, Long> {
 
