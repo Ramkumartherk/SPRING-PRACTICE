b2d9a8adc1b0851a15f4696c98c6e80c4e978eb9
RAMKUMAR S <118542194+Ramkumartherk@users.noreply.github.com>
2023-09-29 08:35:56 +0530

    Update personalpetDAO.java

--- src/main/java/com/cl/cs1/dao/personalpetDAO.java
+++ src/main/java/com/cl/cs1/dao/personalpetDAO.java
@@ -2,7 +2,7 @@ package com.cl.cs1.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository; 
 
-import com.hcl.cs1.model.personalpets;
+import com.cl.cs1.model.personalpets;
 
 public interface personalpetDAO extends JpaRepository<personalpets, Long> {
 
