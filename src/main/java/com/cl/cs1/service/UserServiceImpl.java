3104cb85f9fa01aa6f1395b5e76d434c21e6def4
RAMKUMAR S <118542194+Ramkumartherk@users.noreply.github.com>
2023-09-29 08:45:13 +0530

    Update UserServiceImpl.java

--- src/main/java/com/cl/cs1/service/UserServiceImpl.java
+++ src/main/java/com/cl/cs1/service/UserServiceImpl.java
@@ -8,9 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.hcl.cs1.dao.RoleDAO;
-import com.hcl.cs1.dao.UserDAO;
-import com.hcl.cs1.model.User;
+import com.cl.cs1.dao.RoleDAO;
+import com.cl.cs1.dao.UserDAO;
+import com.cl.cs1.model.User;
 
 @Service
 public class UserServiceImpl implements UserService{
